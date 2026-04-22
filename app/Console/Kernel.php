a7c2109a4558db7ae98eeb79fdf90b9610946132
Tahaahm <tahaahmad837@gmail.com>
2026-04-22 15:18:04 +0300

    put scdule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Http;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,7 +13,12 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            Http::timeout(1800)
+                ->post('http://127.0.0.1:8001/re/train', [
+                    'hyperparameters' => []
+                ]);
+        })->weekly()->sundays()->at('03:00');
     }
 
     /**
@@ -20,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
