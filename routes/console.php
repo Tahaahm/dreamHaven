30fca46263c1e54851e6089270d45e160951950d
Tahaahm <tahaahmad837@gmail.com>
2026-06-17 01:35:52 +0300

    adding Collaborative Filtering & Urgency + Price Drop

--- routes/console.php
+++ routes/console.php
@@ -14,3 +14,16 @@ Schedule::job(new \App\Jobs\FlushPendingFeedNotificationsJob)
 Schedule::command('queue:work --queue=notifications,default --stop-when-empty')
     ->hourly()
     ->withoutOverlapping();
+
+Schedule::job(new \App\Jobs\SendPropertyMatchDigestJob)
+    ->everyThreeDays()
+    ->at('09:00')
+    ->withoutOverlapping();
+
+Schedule::job(new \App\Jobs\PriceDropDetectionJob)
+    ->dailyAt('08:00')
+    ->withoutOverlapping();
+
+Schedule::job(new \App\Jobs\ResurfaceReminderJob)
+    ->cron('0 10 */3 * *')
+    ->withoutOverlapping();
